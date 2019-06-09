b767613b9cc7ab95bde813d4fc0c209782a41568
JunKIyoshi <junkiyoshi@gmail.com>
2019-06-09 19:28:48 +0900

    First

---
+++ ofApp.cpp
@@ -0,0 +1,76 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetColor(39);
+	ofSetLineWidth(3);
+	ofEnableDepthTest();
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	this->cam.begin();
+	ofRotateX(-90);
+
+	auto size = 50;
+	int param_start = ofGetFrameNum() * 0.5;
+	for (int z = -180; z <= 180; z += 60) {
+
+		param_start += 5;
+		for (int param = param_start; param < param_start + 25; param += 5) {
+
+			auto location = glm::vec3(this->make_point(380, param), z);
+
+			ofFill();
+			ofSetColor(239);
+			ofDrawBox(location, size - 2);
+
+			ofNoFill();
+			ofSetColor(39);
+			ofDrawBox(location, size);
+		}
+	}
+
+	this->cam.end();
+}
+
+//--------------------------------------------------------------
+glm::vec2 ofApp::make_point(int len, int param) {
+
+	param = param % 100;
+	if (param < 25) {
+
+		return glm::vec2(ofMap(param, 0, 25, -len * 0.5, len * 0.5), -len * 0.5);
+	}
+	else if (param < 50) {
+
+		return glm::vec2(len * 0.5, ofMap(param, 25, 50, -len * 0.5, len * 0.5));
+	}
+	else if (param < 75) {
+
+		return glm::vec2(ofMap(param, 50, 75, len * 0.5, -len * 0.5), len * 0.5);
+	}
+	else {
+
+		return glm::vec2(-len * 0.5, ofMap(param, 75, 100, len * 0.5, -len * 0.5));
+	}
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
